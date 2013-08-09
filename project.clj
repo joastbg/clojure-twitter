9bc84a3138e85972906b9feb3fbcb4895a44a9ba
Johan Astborg <joastbg@gmail.com>
2013-08-09 22:40:54 +0200

    Added natural language processing

--- project.clj
+++ project.clj
@@ -5,6 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
 	[org.clojure/clojure "1.4.0"]
+  [clojure-opennlp "0.3.1"]
+  [clojurewerkz/titanium "1.0.0-beta1"]
+  [clojurewerkz/cassaforte "1.1.0"]
+  [org.apache.httpcomponents/httpclient "4.1.2"]
+  [org.clojure/clojure-contrib "1.2.0"]
+  [clj-json "0.2.0"]
 	[twitter-api "0.7.4"]]
   :main twitter-connection.core
   :plugins [[lein2-eclipse "2.0.0"]])
--- src/twitter_connection/core.clj
+++ src/twitter_connection/core.clj
@@ -3,7 +3,19 @@
    [twitter.oauth]
    [twitter.callbacks]
    [twitter.callbacks.handlers]
-   [twitter.api.restful])
+   [twitter.api.restful]
+   [clojurewerkz.cassaforte.cql]
+   [clojurewerkz.cassaforte.query])
+  (:require [opennlp.nlp :as nlp]
+   [opennlp.treebank :as tb]
+	 [opennlp.tools.filters :as nlp-tools]
+   [clojurewerkz.titanium.graph    :as tg]
+	 [clojurewerkz.titanium.edges    :as te]
+	 [clojurewerkz.titanium.vertices :as tv]
+	 [clojurewerkz.titanium.types    :as tt]
+	 [clojurewerkz.titanium.query    :as tq]
+   [clojurewerkz.cassaforte.client :as cass]
+   [clj-json.core :as json])
   (:import
    (twitter.callbacks.protocols SyncSingleCallback)))
 
@@ -26,6 +38,96 @@
 (map (first (:body apa)) [:text :created_at])
 ; ((juxt :text :created_at) (first (:body apa)))
 
+;; natural language processing
+(def get-sentences (nlp/make-sentence-detector "models/en-sent.bin"))
+(def tokenize (nlp/make-tokenizer "models/en-token.bin"))
+;(def detokenize (nlp/make-detokenizer "models/english-detokenizer.xml"))
+(def pos-tag (nlp/make-pos-tagger "models/en-pos-maxent.bin"))
+(def chunker (tb/make-treebank-chunker "models/en-chunker.bin"))
+
+(defn test-a
+	[]
+	(pos-tag (tokenize "Mr. Smith gave a car to his son on Friday.")))
+
+(test-a)
+
+;; Extract nouns and verbs
+(nlp-tools/nouns (pos-tag (tokenize "Mr. Smith gave a car to his son on Friday.")))
+(nlp-tools/verbs (pos-tag (tokenize "Mr. Smith gave a car to his son on Friday.")))
+
+;; Categorizer, use Open NLP
+
+;; Titanium, graph database using BercleyDB or Cassandra as storage engine
+(def conf {;; Embedded cassandra settings
+           "storage.backend" "berkeleyje"
+           "storage.directory" "/tmp/graph"})
+           ;;"storage.cassandra-config-dir" cs-dir
+           ;; Embedded elasticsearch settings
+           ;;"storage.index.search.backend" "elasticsearch"
+           ;;"storage.index.search.directory" "/tmp/cassandra/elasticsearch"
+           ;;"storage.index.search.client-only" false
+           ;;"storage.index.search.local-mode" true})
+
+(tg/open (System/getProperty "java.io.tmpdir"))
+
+;; Create vertice
+(tg/transact! (tv/create! {:name "Michael" :location "Europe"}))
+
+;; Create edges
+(tg/transact!
+ (let [p1 (tv/create! {:title "ClojureWerkz" :url "http://clojurewerkz.org"})
+       p2 (tv/create! {:title "Titanium"     :url "http://titanium.clojurewerkz.org"})]
+   (te/connect! p1 :meaningless p2)
+   (te/connect! p1 :meaningful  p2 {:verified-on "February 11th, 2013"})))
+
+(tg/transact! 
+ (tt/defkey :age Long {:indexed-vertex? true :unique-direction :out}))
+
+(tg/transact! 
+ (tv/to-map (tv/create! {:name "Zack"   :age 22})))
+
+(tg/transact! 
+ (tv/create! {:name "Trent"  :age 22}))
+
+(tg/transact! 
+ (tv/create! {:name "Vivian" :age 19}))
+
+(tg/transact! (tv/to-map (first (tv/find-by-kv :age 22))))
+
+(tg/transact!
+  (doseq [x (first (tv/find-by-id :age 22))] (.toString x)))
+
+(tv/id-of a) 
+
+(tv/keys ab)
+
+(tg/transact! (tv/get :name "Zack"))
+
+;; Example of creating vertices, edges and fetching relations
+(tg/transact!
+     (let [from-node (tv/create! {:url "http://clojurewerkz.org/"})
+           to-node   (tv/create! {:url "http://clojurewerkz.org/about.html"})
+           created   (te/connect! from-node :links to-node)
+           fetched   (te/find-by-id  (te/id-of created))]
+     (print (map tv/to-map [from-node to-node created fetched]))
+     (print "The URL:" (:url (tv/to-map from-node)))))       
+
+(tg/transact!
+  (println (count (tv/get-all-vertices)) (count (te/get-all-edges))))
+
+(tg/transact!
+  (tv/get-all-vertices))
+
+;;;;;;;;;;;; Cassandra
+(cass/connect! ["127.0.0.1"])
+
+;;;;;;;;;;;; JSON
+(json/generate-string {:name "Johan" :city "Lund"})
+
+;;;;;;;;;;;; Sample data
+
+;;;;;;;;;;;; Redis
+;; Use redis for sessions
 
 
 (defn foo
